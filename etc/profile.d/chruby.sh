044b480cb923e7291daa1b04fafe3163b523d12b
Postmodern <postmodern.mod3@gmail.com>
2012-10-11 23:20:46 -0700

    Updated the chruby config.

--- etc/profile.d/chruby.sh
+++ etc/profile.d/chruby.sh
@@ -1,8 +1,8 @@
 . /usr/local/etc/profile.d/chruby.sh
 
 RUBIES=(
-  /usr/local/ruby-1.8.7
-  /usr/local/jruby-1.6.7.2
-  /usr/local/jruby-1.7.0-preview2
+  # /usr/local/ruby-1.8.7
+  /usr/local/jruby-1.6.8
+  /usr/local/jruby-1.7.0-rc1
   /usr/local/rubinius
 )
