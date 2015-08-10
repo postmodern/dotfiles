b935288e63fb315ec19eff06085cdef308d1f6bd
Postmodern <postmodern.mod3@gmail.com>
2015-08-09 23:24:54 -0700

    Updated the chruby config.

--- etc/profile.d/chruby.sh
+++ etc/profile.d/chruby.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-source /usr/local/share/chruby/chruby.sh
-
-RUBIES=(/opt/rubies/*)
+if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+	source /usr/local/share/chruby/chruby.sh
+	source /usr/local/share/chruby/auto.sh
+fi
