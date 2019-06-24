e7f05983a3d8c1130340e47c1ba989bf36fe2ea2
Postmodern <postmodern.mod3@gmail.com>
2019-06-24 15:55:27 -0700

    Check if chruby is installed before sourcing the files.

--- etc/profile.d/chruby.sh
+++ etc/profile.d/chruby.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
-	source /usr/local/share/chruby/chruby.sh
-	source /usr/local/share/chruby/auto.sh
+if [ -d /usr/local/share/chruby ]; then
+	if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
+		source /usr/local/share/chruby/chruby.sh
+		source /usr/local/share/chruby/auto.sh
+	fi
 fi
